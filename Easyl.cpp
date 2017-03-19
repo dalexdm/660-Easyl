3e43f285745acdba3103943d76e2fa81b2bbe2d4
Daniel Daley-Montgomery <AlexanderDM@live.com>
2017-03-19 14:20:32 -0400

    Temporary Fur Demo for Tomorrow, Provisions for Future Tool Menu
    Commented methods in paintContextCmd class will be necessary for tool
    menu

--- Easyl.cpp
+++ Easyl.cpp
@@ -24,6 +24,9 @@
 #include <maya/MPoint.h>
 #include <maya/MColor.h>
 
+#define kStartLevelSetFlag "-r"
+#define kStartLevelSetFlagLong "-root"
+
 //////////////////////////////////////////////
 // The user Context
 //////////////////////////////////////////////
@@ -45,6 +48,13 @@ public:
 	virtual MStatus	doRelease( MEvent & event, MHWRender::MUIDrawManager& drawMgr, const MHWRender::MFrameContext& context);
 	virtual MStatus	doDrag ( MEvent & event, MHWRender::MUIDrawManager& drawMgr, const MHWRender::MFrameContext& context);
 
+	//tool Settings methods
+	void setStartLevel(int level);
+	int getStartLevel() { return startLevel; };
+
+	//temporary
+	bool isFeather = false;
+
 private:
 	// Press and Release shared operations (agnostic of viewport)
 	void doPressCommon( MEvent & event );
@@ -56,6 +66,7 @@ private:
 	std::vector<float> tValues;
 	//Temporary screen locations
 	short lastx, lasty;
+	int startLevel;
 	// screen space object
 	M3dView view;
 };
@@ -63,7 +74,7 @@ private:
 paintContext::paintContext()
 {
 	setTitleString ( "Easyl" );
-
+	startLevel = 0;
 	// Tell the context which XPM to use so the tool can properly
 	// be a candidate for the 6th position on the toolbar.
 	setImage("Easyl.xpm", MPxContext::kImage1 );
@@ -98,7 +109,7 @@ void paintContext::doPressCommon( MEvent & event )
 }
 
 void paintContext::makeCurve() {
-	
+
 	MStatus s;
 
 	//get the mesh
@@ -108,35 +119,64 @@ void paintContext::makeCurve() {
 	//currently operates ONLY on the first mesh it finds
 	MFnMesh mesh(meshObj, &s);
 
-	//determine t values for every i
-	for (int i = 0; i < origins.size(); i++) {
-		float error = 10000;
-		float lastError = 10000;
-		
-		//check if we can rely on a converging error
-		if (mesh.intersect(origins[i], directions[i], MPointArray(), &s)) {
-			while (true) {
-				MPoint closestPoint;
-				MPoint thisPoint = origins[i] + tValues[i] * directions[i];
-				mesh.getClosestPoint(thisPoint, closestPoint);
-				error = thisPoint.distanceTo(closestPoint);
-				if (error < kMaxError) break;
-				tValues[i] += error;
+	if (!isFeather) {
+		//determine t values for every i
+		for (int i = 0; i < origins.size(); i++) {
+			float error = 10000;
+			float lastError = 10000;
+
+			//check if we can rely on a converging error
+			if (mesh.intersect(origins[i], directions[i], MPointArray(), &s)) {
+				while (true) {
+					MPoint closestPoint;
+					MPoint thisPoint = origins[i] + tValues[i] * directions[i];
+					mesh.getClosestPoint(thisPoint, closestPoint);
+					error = thisPoint.distanceTo(closestPoint);
+					if (error < kMaxError) break;
+					tValues[i] += error;
+				}
+				//if not get near the inflection - REALLY raw right now
 			}
-		//if not get near the inflection - REALLY raw right now
-		} else {
+			else {
+				while (true) {
+					MPoint closestPoint;
+					MPoint thisPoint = origins[i] + tValues[i] * directions[i];
+					mesh.getClosestPoint(thisPoint, closestPoint);
+					error = thisPoint.distanceTo(closestPoint);
+					if (error > lastError) {
+						break;
+					}
+					tValues[i] += error;
+					lastError = error;
+
+				}
+			}
+		}
+
+	//temporary feather demo - will be replaced by optimization method and custom tool options
+	} else {
+		//determine t values for first and last i
+		for (int i = 0; i < origins.size(); i+=origins.size() - 1) {
+			float error = 10000;
+			float lastError = 10000;
 			while (true) {
 				MPoint closestPoint;
 				MPoint thisPoint = origins[i] + tValues[i] * directions[i];
 				mesh.getClosestPoint(thisPoint, closestPoint);
 				error = thisPoint.distanceTo(closestPoint);
-				if (error > lastError) {
+				if (i > 0) error -= 0.5;
+				MGlobal::displayInfo(MString() + error + ", " + lastError);
+				if (error > lastError || error < 0.05) {
 					break;
 				}
 				tValues[i] += error;
 				lastError = error;
 
 			}
+		}
+
+		for (int i = 1; i < origins.size() - 1; i++) {
+			tValues[i] = ((float)i / (float)origins.size()) * (tValues[origins.size()-1] - tValues[0]) + tValues[0];
 		}
 	}
 
@@ -235,6 +275,10 @@ MStatus	paintContext::doDrag (MEvent & event, MHWRender::MUIDrawManager& drawMgr
 	return MS::kSuccess;
 }
 
+void paintContext::setStartLevel(int theLevel) {
+	startLevel = theLevel;
+}
+
 //////////////////////////////////////////////
 // Command to create contexts
 //////////////////////////////////////////////
@@ -245,19 +289,84 @@ public:
 						paintContextCmd();
 	virtual MPxContext*	makeObj();
 	static	void*		creator();
+	//temporary, until the tool menu mentioned below is implemented
+	static  void*		creator2();
+	bool				isFeather = false;
+
+	/* Will eventually be used for tool menu *paired with commented methods below*
+	virtual	MStatus		doEditFlags();
+	virtual MStatus		doQueryFlags();
+	virtual MStatus		appendSyntax();
+	*/
+protected:
+	paintContext*		fPaintContext;
+	//temporary, until the tool menu mentioned above is implemented
 };
 
 paintContextCmd::paintContextCmd() {}
 
 MPxContext* paintContextCmd::makeObj()
 {
-	return new paintContext();
+	fPaintContext = new paintContext();
+	if (isFeather) fPaintContext->isFeather = true;
+	return fPaintContext;
 }
 
 void* paintContextCmd::creator()
 {
 	return new paintContextCmd;
 }
+
+void* paintContextCmd::creator2() {
+	paintContextCmd* output = new paintContextCmd();
+	output->isFeather = true;
+	return output;
+}
+
+//Will Eventually be used for setting up tool menu *paired with commented methods in header*
+/*
+MStatus paintContextCmd::doEditFlags()
+{
+	MStatus status = MS::kSuccess;
+
+	MArgParser argData = parser();
+
+	if (argData.isFlagSet(kStartLevelSetFlag)) {
+		int level;
+		status = argData.getFlagArgument(kStartLevelSetFlag, 0, level);
+		if (!status) {
+			status.perror("numCVs flag parsing failed.");
+			return status;
+		}
+		fPaintContext->setStartLevel(level);
+	}
+
+	return MS::kSuccess;
+}
+
+MStatus paintContextCmd::doQueryFlags()
+{
+	MArgParser argData = parser();
+
+	if (argData.isFlagSet(kStartLevelSetFlag)) {
+		setResult((int)fPaintContext->getStartLevel());
+	}
+
+	return MS::kSuccess;
+}
+
+MStatus paintContextCmd::appendSyntax()
+{
+	MSyntax mySyntax = syntax();
+
+	if (MS::kSuccess != mySyntax.addFlag(kStartLevelSetFlag, kStartLevelSetFlagLong,
+		MSyntax::kLong)) {
+		return MS::kFailure;
+	}
+
+	return MS::kSuccess;
+}
+*/
 
 //////////////////////////////////////////////
 // plugin initialization
@@ -267,8 +376,18 @@ MStatus initializePlugin( MObject obj )
 	MStatus		status;
 	MFnPlugin	plugin( obj, PLUGIN_COMPANY, "12.0", "Any");
 
+	//auto-load the UI path and add it to MAYA_SCRIPT_PATH
+	MString pathPrefix = plugin.loadPath();
+	MGlobal::executeCommand("string $paat = \"" + pathPrefix + "\";");
+	MGlobal::executeCommand("string $compat = $paat + \";\" + getenv(\"MAYA_SCRIPT_PATH\");");
+	MGlobal::executeCommand("putenv \"MAYA_SCRIPT_PATH\" $compat;");
+
 	status = plugin.registerContextCommand( "paintContext",
 										    paintContextCmd::creator );
+
+	status = plugin.registerContextCommand("featherPaintContext",
+		paintContextCmd::creator2);
+
 	return status;
 }
 
